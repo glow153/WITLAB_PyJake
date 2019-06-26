f5f74a668583c008b7c2e0cf41b2bd3c76acdc7e
Jake Park <glow153@gmail.com>
2019-06-26 09:54:06 +0900

    add comment

--- geo/api_kakao_geocode.py
+++ geo/api_kakao_geocode.py
@@ -5,10 +5,22 @@ import requests
 
 def getLatLon(address: str):
     """
-    important issues
-        1. API 호출횟수 제한이 있으므로 사용에 유의
+    * api info
+    GET /v2/local/search/address.{format} HTTP/1.1
+    Host: dapi.kakao.com
+    Authorization: KakaoAK {app_key}
+    request
+    키	설명                            필수    	타입
+    query	검색을 원하는 질의어(주소)     	O        	String
+    page	결과 페이지 번호            	X(기본 1)	Integer
+    size	한 페이지에 보여질 문서의 개수	X(기본 10)	1-30 사이 Integer
+
+    * usage
     :param address: 위경도를 알아내고자 하는 대상의 주소 (도로명주소 권장)
     :return: tuple(lat: float, lon: float)
+
+    * important issues
+        1. API 호출횟수 제한이 있으므로 사용에 유의
     """
     # address_test = '천안대로 1223-24'
     apikey = 'cf606c4c1964ec437d0134cbb5a8deb9'
